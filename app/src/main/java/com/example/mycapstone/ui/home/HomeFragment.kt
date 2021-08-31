50fc89fb528f98f8b52fa886fa6cadd528b3fa1f
senta2006 <senta2006@naver.com>
2021-09-01 01:15:46 +0900

    fix: viewModel 구조 변경

--- app/src/main/java/com/example/mycapstone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mycapstone/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mycapstone.api.Api
 import com.example.mycapstone.PolicyAdapter
@@ -25,13 +26,11 @@ import retrofit2.Retrofit
 class HomeFragment : Fragment() {
     private lateinit var binding: HomeFragmentBinding
     val viewModel: HomeViewmodel by viewModels()
-
-    private val mDatas = mutableListOf<Policy>()
     var policyAdapter: PolicyAdapter = PolicyAdapter()
 
     private var api: Api? = null
     private var retrofit: Retrofit? = null
-    val key = "WNKS76ZZ47R04LNMS88MK2VR1HJ"
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,13 +45,9 @@ class HomeFragment : Fragment() {
             false
         )
 
-        // 처음에 값 추가
-//        mDatas.add(Policy("a", "a", "a", "a"))
-
         binding.rvPolicy.layoutManager =
             LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         binding.rvPolicy.adapter = policyAdapter
-        //policyAdapter.data = mDatas
 
         binding.viewModel = viewModel
         return binding.root
@@ -63,64 +58,27 @@ class HomeFragment : Fragment() {
 
         retrofit = RetrofitClient.getInstance()
         api = retrofit?.create(Api::class.java)
-        getPolicy("PLCYTP040001")
+
+        viewModel.mDatas.observe(viewLifecycleOwner, {
+            // 추가한 data 대입
+            policyAdapter.data = it as MutableList<Policy>
+            // data 변경 알림
+            policyAdapter.notifyDataSetChanged()
+        })
+
+        viewModel.getPolicy(api, "PLCYTP040001")
 
         binding.btmNavi.setOnItemSelectedListener {
-            mDatas.clear()
-            binding.rvPolicy.scrollToPosition(1)
+            //
+            binding.rvPolicy.scrollToPosition(0)
             when (it.title) {
-                "생활/금융" -> getPolicy("PLCYTP040001")
-                "창업" -> getPolicy("PLCYTP020002")
-                "취업" -> getPolicy("PLCYTP01")
-                "주거/교통" -> getPolicy("PLCYTP040002")
-                "문화" -> getPolicy("PLCYTP030002")
+                "생활/금융" -> viewModel.getPolicy(api, "PLCYTP040001")
+                "창업" -> viewModel.getPolicy(api, "PLCYTP020002")
+                "취업" -> viewModel.getPolicy(api, "PLCYTP01")
+                "주거/교통" -> viewModel.getPolicy(api, "PLCYTP040002")
+                "문화" -> viewModel.getPolicy(api, "PLCYTP030002")
             }
-
             return@setOnItemSelectedListener true
-        }
-
-        // 추가한 data 대입
-        //policyAdapter.data = mDatas
-        // data 변경 알림
-        //policyAdapter.notifyDataSetChanged()
-    }
-
-    private fun getPolicy(policyType: String) {
-        try {
-            val callResult = api?.getInfo(key, "xml", 1, 10, policyType)
-
-            callResult?.enqueue(object : Callback<jynEmpSptRoot> {
-                override fun onResponse(
-                    call: Call<jynEmpSptRoot>,
-                    response: Response<jynEmpSptRoot>
-                ) {
-                    Log.i("SUCCESS !! ", "${response.raw()}")
-                    for (i in 0..9) {
-                        mDatas.add(
-                            Policy(
-                                response.body()?.jynEmpSptList?.get(i)?.busiNm,
-                                response.body()?.jynEmpSptList?.get(i)?.busiTpCd,
-                                response.body()?.jynEmpSptList?.get(i)?.busiId,
-                                response.body()?.jynEmpSptList?.get(i)?.age,
-                                response.body()?.jynEmpSptList?.get(i)?.dtlBusiNm,
-                                response.body()?.jynEmpSptList?.get(i)?.edubgEtcCont,
-                                response.body()?.jynEmpSptList?.get(i)?.empEtcCont,
-                                response.body()?.jynEmpSptList?.get(i)?.detalUrl,
-                            )
-                        )
-                    }
-                    // 추가한 data 대입
-                    policyAdapter.data = mDatas
-                    // data 변경 알림
-                    policyAdapter.notifyDataSetChanged()
-                }
-
-                override fun onFailure(call: Call<jynEmpSptRoot>, t: Throwable) {
-                    Log.e("call Error: ", "${t.printStackTrace()}")
-                }
-            })
-        } catch (e: Exception) {
-            Log.e("Error: ", "$e")
         }
     }
 }
--- app/src/main/java/com/example/mycapstone/ui/home/HomeViewmodel.kt
+++ app/src/main/java/com/example/mycapstone/ui/home/HomeViewmodel.kt
@@ -1,6 +1,58 @@
 package com.example.mycapstone.ui.home
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.mycapstone.api.Api
+import com.example.mycapstone.data.Policy
+import com.example.mycapstone.data.jynEmpSptRoot
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class HomeViewmodel:ViewModel(){
+class HomeViewmodel : ViewModel() {
+    private val key = "WNKS76ZZ47R04LNMS88MK2VR1HJ"
+
+    private val _mDatas = MutableLiveData<List<Policy>>()
+    val mDatas:LiveData<List<Policy>> = _mDatas
+
+    fun getPolicy(api: Api?, policyType: String) {
+        val policyList = mutableListOf<Policy>()
+
+        try {
+            val callResult = api?.getInfo(key, "xml", 1, 10, policyType)
+
+            callResult?.enqueue(object : Callback<jynEmpSptRoot> {
+                override fun onResponse(
+                    call: Call<jynEmpSptRoot>,
+                    response: Response<jynEmpSptRoot>
+                ) {
+                    Log.i("SUCCESS !! ", "${response.raw()}")
+                    for (i in 0..9) {
+                        policyList.add(
+                            Policy(
+                                response.body()?.jynEmpSptList?.get(i)?.busiNm,
+                                response.body()?.jynEmpSptList?.get(i)?.busiTpCd,
+                                response.body()?.jynEmpSptList?.get(i)?.busiId,
+                                response.body()?.jynEmpSptList?.get(i)?.age,
+                                response.body()?.jynEmpSptList?.get(i)?.dtlBusiNm,
+                                response.body()?.jynEmpSptList?.get(i)?.edubgEtcCont,
+                                response.body()?.jynEmpSptList?.get(i)?.empEtcCont,
+                                response.body()?.jynEmpSptList?.get(i)?.detalUrl,
+                            )
+                        )
+                    }
+                    _mDatas.value = policyList
+                }
+
+                override fun onFailure(call: Call<jynEmpSptRoot>, t: Throwable) {
+                    Log.e("call Error: ", "${t.printStackTrace()}")
+                }
+            })
+
+        } catch (e: Exception) {
+            Log.e("Error: ", "$e")
+        }
+    }
 }
