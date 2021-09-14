4a8e6a0b3673e49e9f4bef06ab22c13a9889ec5f
senta2006 <senta2006@naver.com>
2021-09-15 00:30:27 +0900

    add: 구글 로그인 추가

--- app/src/main/java/com/example/mycapstone/ui/setting/login/LoginFragment.kt
+++ app/src/main/java/com/example/mycapstone/ui/setting/login/LoginFragment.kt
@@ -1,26 +1,98 @@
 package com.example.mycapstone.ui.setting.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.mycapstone.R
 import com.example.mycapstone.databinding.LoginFragmentBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginFragment : Fragment() {
     val viewModel: LoginViewModel by viewModels()
     lateinit var binding: LoginFragmentBinding
 
+    private val webClientId = "633392197313-ougjvtocqosbff7hde9ghtck73ks2bhi.apps.googleusercontent.com"
+    private lateinit var client: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private val requestCode = 1
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
         binding.vm = viewModel
+        binding.loginButton.setOnClickListener { setGoogleLogin() }
+        binding.logoutButton.setOnClickListener { logout() }
+
         return binding.root
+    }
+
+
+    private fun logout() {
+        FirebaseAuth.getInstance().signOut()
+        Toast.makeText(requireContext(), "로그아웃에 성공했습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun setGoogleLogin() {
+        // 요청 정보 옵션
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(webClientId)
+            .requestEmail().build()
+        client = GoogleSignIn.getClient(requireActivity(), gso)
+        firebaseAuth = FirebaseAuth.getInstance()
+        // 로그인 요청
+        startActivityForResult(client.signInIntent, requestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == this.requestCode) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!.idToken)
+            } catch (e: ApiException) {
+                Toast.makeText(requireContext(), "Failed Google Login", Toast.LENGTH_SHORT).show()
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            ?.addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // 인증에 성공한 후, 로그인된 유저 정보 가져오기
+                    val email = firebaseAuth.currentUser?.email
+
+                    email?.let { em -> Log.i("사용자 이메일: ", em) }
+                    Toast.makeText(
+                        requireContext(),
+                        "${firebaseAuth.currentUser?.displayName} 님 안녕하세요!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(requireContext(), "로그인에 실패했습니다", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
